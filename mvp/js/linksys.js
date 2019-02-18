2d0b6c2a6e1070f41f7c78a1b9f0181cdaad5416
nhj048 <nhj048@gmail.com>
2019-02-18 19:18:54 +0900

    상세페이지 작업

--- mvp/js/linksys.js
+++ mvp/js/linksys.js
@@ -1,3 +1,10 @@
+// linksys.js
+
 $(function(){
-		
+	$(".ca-link").click(function(e) {
+		url = "/www/mvp/ca_view.html";
+		window.open(url, "PopupWin", "width=1200, height=920, scrollbar=yes, resizable=yes");
+	
+	});
+
 });// jQB //
