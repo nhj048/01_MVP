a1583c1f607ab62dfe35ecec8b56c0b76bcf518c
nhj048 <46480308+nhj048@users.noreply.github.com>
2019-02-20 02:20:19 +0900

    집에서 수정 함!

--- mvp/js/linksys.js
+++ mvp/js/linksys.js
@@ -2,7 +2,7 @@
 
 $(function(){
 	$(".ca-link").click(function(e) {
-		url = "/www/mvp/ca_view.html";
+		url = "/www/mvp/ca_view2.html";
 		window.open(url, "PopupWin", "width=1200, height=920, scrollbar=yes, resizable=yes");
 	
 	});
